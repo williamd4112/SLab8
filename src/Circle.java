41fdeeb98ce3c7855d4bb91f27941da8b6898b36
williamd4112 <williamd4112@hotmail.com>
2014-12-02 17:02:45 +0800

    Some fix

--- src/Circle.java
+++ src/Circle.java
@@ -3,11 +3,15 @@ public class Circle {
 	
 	static final double PI = 3.141592653589793;
 	
-	public static void main(String[] args){
-		
-		double radius = 3 , area;
-		
-		area = PI * radius * radius;
-		System.out.println("Circle area is:" + area);
+	private double radius;
+	
+	public Circle(double radius)
+	{
+		this.radius = radius;
+	}
+	
+	public void showArea()
+	{
+		System.out.println(this.radius * this.radius * PI);
 	}
 }
--- src/Square.java
+++ src/Square.java
@@ -1,9 +1,22 @@
 
 public class Square {
 
+	private double length;
+	
+	public Square(double length)
+	{
+		this.length = length;
+	}
+	
+	public void showArea()
+	{
+		System.out.println(this.length * this.length);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Square(10).showArea();
+		new Circle(10).showArea();
 	}
 
 }
