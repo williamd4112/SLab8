512c563309d817335043669601db7f85a2c81bf5
tso0159753 <tso0159753@gmail.com>
2014-12-02 16:54:04 +0800

    Circle

---
+++ src/Circle.java
@@ -0,0 +1,13 @@
+
+public class Circle {
+	
+	static final double PI = 3.141592653589793;
+	
+	public static void main(String[] args){
+		
+		double radius = 3 , area;
+		
+		area = PI * radius * radius;
+		System.out.println("Circle area is:" + area);
+	}
+}
